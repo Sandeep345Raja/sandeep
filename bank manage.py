5d1c081d18e8a6cc23edc12280df46b269fceadc
Sandeep345Raja <129730453+Sandeep345Raja@users.noreply.github.com>
2023-04-03 13:29:56 +0530

    Add files via upload

---
+++ bank manage.py
@@ -0,0 +1,11 @@
+from tkinter import *
+class login:
+    def__init__(self,root):
+     self.root = root
+    self.root.tittle("login system")
+    self.root.geometry("1199x600+100+50")
+
+
+root = TK()
+obj = login(root)
+root.mainloop()
